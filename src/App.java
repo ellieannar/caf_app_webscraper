5b124d32489b7c5d59c29178f6e0d13e2c34edad
ellieannar <ellieanna.ross@hotmail.com>
2023-11-29 11:10:04 -0800

    Simplifying and organizing App.java

--- src/App.java
+++ src/App.java
@@ -1,114 +1,132 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+
+/*
+* This is the information regarding how to locate certain parts of data through the site
+* this is the tab: System.out.println((item.parent().parent().parent().parent().parent().parent()));
+* this is the meal: System.out.println("\t" + item.parent().parent().parent().parent().parent().parent().parent().parent().parent().parent().attributes());
+* this is the location: item.parent().parent().parent().parent().getElementsByTag("h3").text()
+* these are the dietary restrictions: Elements dietary = item.child(0).children();
+* this is the info about the item: System.out.println("\t" + item.parent().parent().child(1).text());
+*/
+
 public class App {
 
     public static void main(String[] args)  {
 
-            Scanner scanner = new Scanner(System.in);
-            boolean flag = true;
-            String dateString = "";
-            
-            System.out.print("Enter the date in the following format yyyy-mm-dd: ");
+        // Get the date from the user
+        Scanner scanner = new Scanner(System.in);
+        String dateString = "";
         
-            dateString = scanner.nextLine();
-            dateString.concat("/");
-                
+        System.out.print("Enter the date in the following format yyyy-mm-dd: ");
+    
+        dateString = scanner.nextLine();
+        dateString.concat("/");
 
-        
-       
-         
-        // https://cafebiola.cafebonappetit.com/cafe/cafe-biola/2023-11-28/
+        scanner.close();
+
+        //Set up the url to access - use the dateString to specify
         final String biolaUrl = "https://cafebiola.cafebonappetit.com/cafe/cafe-biola/".concat(dateString);
 
+        //Day to hold current day
         Day today = new Day();
+
+        // Attempt to pull the data
         try {
+
+            //get the site information
             final Document biolaSite = Jsoup.connect(biolaUrl).get();
 
-            //this is the tab: System.out.println((item.parent().parent().parent().parent().parent().parent()));
-            // this is the meal: System.out.println("\t" + item.parent().parent().parent().parent().parent().parent().parent().parent().parent().parent().attributes());
-            // this is the location: item.parent().parent().parent().parent().getElementsByTag("h3").text()
-            // these are the dietary restrictions: Elements dietary = item.child(0).children();
-            // this is the info about the item: System.out.println("\t" + item.parent().parent().child(1).text());
-             
+            //All the food items for the given day
             Elements foodItems = biolaSite.getElementsByClass("h4 site-panel__daypart-item-title");
-            //print JUST the elements that are served today
+            
 
             
             for (Element item : foodItems) {
+                //the tab, which needs to be 1
                 Element tab = item.parent().parent().parent().parent().parent().parent();
-                Element meal = tab.parent().parent().parent().parent();
 
-                //Breakfasts
-                if (tab.hasAttr("data-loop-index") && tab.attr("data-loop-index").equals("1") && meal.attr("data-daypart-id").equals("1")) {
-                    FoodItem food = new FoodItem();
-                    food.title = item.text();
-                    // Dietary restrictions associated with food item
-                    if (!item.children().isEmpty()) {
-                        Elements dietary = item.child(0).children();
-                    
-                        for (Element restriction: dietary) {
-                            food.restrictions.add(restriction.attr("title"));
+
+                if (tab.hasAttr("data-loop-index") && tab.attr("data-loop-index").equals("1")) {
+                    //the meal (1 = breakfast, 3 = lunch, 4 = dinner)
+                    Element meal = tab.parent().parent().parent().parent();
+
+                    //Breakfasts
+                    if (meal.attr("data-daypart-id").equals("1")) {
+                        //Temporary food item to keep track of the details
+                        FoodItem food = new FoodItem();
+
+                        food.title = item.text();
+
+                        // Dietary restrictions associated with food item
+                        if (!item.children().isEmpty()) {
+                            Elements dietary = item.child(0).children();
+                            for (Element restriction: dietary) {
+                                food.restrictions.add(restriction.attr("title"));
+                            }
                         }
-                    }
-                    food.description = item.parent().parent().child(1).text();
-                    food.location = item.parent().parent().parent().parent().getElementsByTag("h3").text();
-                    today.breakfast.mealElements.add(food);
-                }
-                 
-                //Lunches
-                else if (tab.hasAttr("data-loop-index") && tab.attr("data-loop-index").equals("1") && meal.attr("data-daypart-id").equals("3")) {
-                    FoodItem food = new FoodItem();
-                    food.title = item.text();
-                    if (!item.children().isEmpty()) {
-                        Elements dietary = item.child(0).children();
-                    
-                        for (Element restriction: dietary) {
-                            food.restrictions.add(restriction.attr("title"));
-                        }
+
+                        food.description = item.parent().parent().child(1).text();
+                        food.location = item.parent().parent().parent().parent().getElementsByTag("h3").text();
+
+                        //add food to breakfast items for today
+                        today.breakfast.mealElements.add(food);
                     }
                     
+                    //Lunches
+                    else if (meal.attr("data-daypart-id").equals("3")) {
+                        //Temporary food item to keep track of the details
+                        FoodItem food = new FoodItem();
+                        food.title = item.text();
 
-                    food.description = item.parent().parent().child(1).text();
-                    food.location = item.parent().parent().parent().parent().getElementsByTag("h3").text();
-                    today.lunch.mealElements.add(food);
-                }
-                
-
-                //Dinners
-                if (tab.hasAttr("data-loop-index") && tab.attr("data-loop-index").equals("1") && meal.attr("data-daypart-id").equals("4")) {
-                    FoodItem food = new FoodItem();
-                    food.title = item.text();
-                    // Dietary restrictions associated with food item
-                    if (!item.children().isEmpty()) {
-                        Elements dietary = item.child(0).children();
-                    
-                        for (Element restriction: dietary) {
-                            food.restrictions.add(restriction.attr("title"));
-                            //System.out.println(restriction.attr("title"));
+                        //Dietary restrictions associated with food item
+                        if (!item.children().isEmpty()) {
+                            Elements dietary = item.child(0).children();
+                        
+                            for (Element restriction: dietary) {
+                                food.restrictions.add(restriction.attr("title"));
+                            }
                         }
+                        
+                        food.description = item.parent().parent().child(1).text();
+                        food.location = item.parent().parent().parent().parent().getElementsByTag("h3").text();
+
+                        // add food to lunch items for today
+                        today.lunch.mealElements.add(food);
                     }
-                    food.description = item.parent().parent().child(1).text();
-                    food.location = item.parent().parent().parent().parent().getElementsByTag("h3").text();
-                    today.dinner.mealElements.add(food);
+                    
+
+                    //Dinners
+                    else if (meal.attr("data-daypart-id").equals("4")) {
+                        //Temporary food item to keep track of the details
+                        FoodItem food = new FoodItem();
+                        food.title = item.text();
+
+                        // Dietary restrictions associated with food item
+                        if (!item.children().isEmpty()) {
+                            Elements dietary = item.child(0).children();
+                            for (Element restriction: dietary) {
+                                food.restrictions.add(restriction.attr("title"));
+                            }
+                        }
+
+                        food.description = item.parent().parent().child(1).text();
+                        food.location = item.parent().parent().parent().parent().getElementsByTag("h3").text();
+
+                        // add food to lunch items for today
+                        today.dinner.mealElements.add(food);
+                    }
+
                 }
-                
 
-
-    
             }
 
-            
+            // Display to log what day looks like
             today.print();
-
-           scanner.close();
            
 
         } catch (Exception ex) {
