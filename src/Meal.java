f11b2e7b07f92f81c6dfe866b187c7c26a9123c1
ellieannar <ellieanna.ross@hotmail.com>
2023-11-29 11:12:55 -0800

    Simplifying Meal.java

--- src/Meal.java
+++ src/Meal.java
@@ -1,12 +1,4 @@
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 public class Meal{
         ArrayList<FoodItem> mealElements = new ArrayList<FoodItem>(); 
