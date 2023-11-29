34136e4baa797a3b2c75e14db0b55ad1ccdb2e09
ellieannar <ellieanna.ross@hotmail.com>
2023-11-29 11:12:21 -0800

    Modifying and organizing FoodItem.java

--- src/FoodItem.java
+++ src/FoodItem.java
@@ -1,15 +1,9 @@
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 
 public class FoodItem {
+
+    // Each food item stores the title, desc, restrictions, and location
     String location;
     ArrayList<String> restrictions = new ArrayList<String>(); 
     String title;
